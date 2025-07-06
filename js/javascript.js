38364d9e8a147043ac94139157a3d0d574c0ecab
IsabellaAdriane <isabellaadrianesouza@gmail.com>
2025-07-06 15:01:56 -0300

    now

--- js/javascript.js
+++ js/javascript.js
@@ -4,4 +4,3 @@ let frase = document.querySelector("#frase");
 botao.addEventListener("click", function mostrarFrase() {
     frase.textContent = "Seja você quem for, seja qual for a posição social que você tenha na vida, a mais alta ou a mais baixa, tenha sempre como meta muita força, muita determinação e sempre faça tudo com muito amor e com muita fé em Deus, que um dia você chega lá. De alguma maneira você chega lá. - Ayrton Senna";
 });
-
